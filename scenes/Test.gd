c030ca8b06caf23a1f624b4edb6ee01bc3ae1e4b
Reed McCallum <14640761+reedmccallum@users.noreply.github.com>
2020-10-16 23:13:07 -0500

    Object click functionality; asset file structure update

---
+++ scenes/Test.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Helmet_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT && event.pressed:
+		print("Helmet pressed")
+		# add Helmet to inventory
+		$Helmet.set_visible(false)
+	pass # Replace with function body.
