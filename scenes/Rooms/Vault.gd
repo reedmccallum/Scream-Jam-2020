a0f4409426f7a866a936ab36335dd106687c8812
Reed McCallum <14640761+reedmccallum@users.noreply.github.com>
2020-10-23 14:20:08 -0500

    fixed chonky cursor; brightened observatory by 20 alpha

--- scenes/Rooms/Vault.gd
+++ scenes/Rooms/Vault.gd
@@ -14,3 +14,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_pile_input_event(_viewport, _event, _shape_idx):
+	if _event is InputEventMouseButton && _event.button_index == BUTTON_LEFT && _event.pressed:
+		$final_scene/AnimationPlayer.play("gold_to_sand")
+	pass # Replace with function body.
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	if _anim_name == "gold_to_sand":
+		$final_scene/AnimationPlayer.play("monster_attacks")
+	pass # Replace with function body.
