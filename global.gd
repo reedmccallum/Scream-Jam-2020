c02ea8c1d7cb62d4ee5ac4ed47b3d6ba6590a488
Reed McCallum <14640761+reedmccallum@users.noreply.github.com>
2020-10-23 16:54:01 -0500

    fixed fullscreen and icon

--- global.gd
+++ global.gd
@@ -19,9 +19,9 @@ func hide_ui():
 func show_exit():
 	$UI.show_exit()
 
-#func _init():
+func _init():
 #	OS.min_window_size = OS.window_size
-#	OS.max_window_size = OS.get_screen_size()
+	OS.max_window_size = OS.get_screen_size()
 
 func _unhandled_input(_event):
 	if _event.is_action_pressed("toggle_fullscreen"):
